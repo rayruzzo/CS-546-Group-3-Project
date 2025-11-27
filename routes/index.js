d19762ace7d0543c5a5a09e512519830d5c6971d
elisavetsky <code.approved@slmail.me>
2025-11-26 23:47:18 -0800

    add `/users` routes to route index

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 import {default as postRouter} from './posts.js'
+import {default as userRouter} from './users.js'
 
 const constructorMethod = (app) => {
     app.use('/posts', postRouter);
+    app.use('/users', userRouter);
 
     app.use((req, res) => {
     return res.status(404).json({error: 'Route Not found'});
