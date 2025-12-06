a456e64d67d5fdc1bb743c727e69d72b801b9ec7
Nicholas <nraghuna@stevens.edu>
2025-12-06 02:13:35 -0500

    Removing Archive and old index.html

--- archive/hub.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-const router = Router();
-
-router.get("/", async (req, res) => {
-    return res.render("hub", { title: "Project Navigation Hub" });
-});
-
-export default router;
