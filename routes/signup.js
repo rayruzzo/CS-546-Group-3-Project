2b71ae552043ab78443b09cc2df762c3bf815231
elisavetsky <code.approved@slmail.me>
2025-12-14 10:58:52 -0800

    update `/routes/signup.js` w/middleware & vars

--- routes/signup.js
+++ routes/signup.js
@@ -1,23 +1,43 @@
 import { Router } from 'express';
 import userFunctions from "../data/users.js";
 import { renderErrorPage } from '../utils/errorUtils.js';
-import { userSchema } from '../models/users.js';
+import { SCHEMA_CONFIG, userSchema } from '../models/users.js';
 import { validateSchema } from '../middleware/validation.mw.js';
+import { shapeSignupData } from '../middleware/signup.mw.js';
 
 const router = Router();
 
 router.get('/', (req, res) => {
     return res.render('signup', {
         title: "Create Account",
+        passwordErrorMessage: SCHEMA_CONFIG.password.message,
+        SCHEMA_CONFIG: {
+            username: {
+                ...SCHEMA_CONFIG.username,
+                regex: SCHEMA_CONFIG.username.regex.toString()
+                            .slice(1, SCHEMA_CONFIG.username.regex.toString().length - 1),
+            },
+            password: {
+                ...SCHEMA_CONFIG.password,
+                regex: SCHEMA_CONFIG.password.regex.toString()
+                            .slice(1, SCHEMA_CONFIG.password.regex.toString().length - 1),
+            },
+            zipcode: {
+                ...SCHEMA_CONFIG.zipcode,
+                regex: SCHEMA_CONFIG.zipcode.regex.toString()
+                            .slice(1, SCHEMA_CONFIG.zipcode.regex.toString().length - 1),
+            },
+        },
         error: null
     });
 });
 
 
 router.post("/", 
-// <——— new data shaping middleware here before validation
+shapeSignupData,
 validateSchema(userSchema, "body"),
 async (req, res) => {
+
     try {
         const {user} = await userFunctions.createUser(req.body);
         req.session.user = {
@@ -27,6 +47,7 @@ async (req, res) => {
             role: user.role,
             username: user.profile?.username || ""
         };
+
         return res.redirect("/");
 
     } catch (error) {
